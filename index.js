a5aac7be94bbcb33f8947f4d9453cbe233d01754
sakshi <jhasakshi639@gmail.com>
2023-09-16 12:05:07 +0530

    my api

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
 const port = process.env.PORT || 3001;
 
 const apiData = require('./data.json')
